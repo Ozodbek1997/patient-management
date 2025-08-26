1a63dc4576b4c776a11eb2cdba7cd684e095ff3f
오조벡 (CT00383) <ozodbek@nextree.io>
2025-08-26 13:02:17 +0900

    adding billing service

--- billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
+++ billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
@@ -1,4 +1,30 @@
 package com.pm.billingservice.grpc;
 
-public class BillingGrpcService {
+
+import billing.BillingRequest;
+import billing.BillingResponse;
+import billing.BillingServiceGrpc;
+import io.grpc.stub.StreamObserver;
+import lombok.extern.slf4j.Slf4j;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@Slf4j
+@GrpcService
+public class BillingGrpcService extends BillingServiceGrpc.BillingServiceImplBase {
+
+    @Override
+    public void createBillingAccount(BillingRequest request,
+                                     StreamObserver<BillingResponse> responseObserver) {
+        log.info("Create billing account request: {}", request.toString());
+
+        //Business logic - e.g save to database, perform calculates etc
+
+        BillingResponse response = BillingResponse.newBuilder()
+                .setAccountId("1234")
+                .setStatus("ACTIVE")
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
 }
--- patient-service/src/main/java/com/pm/patientservice/dto/PatientRequestDTO.java
+++ patient-service/src/main/java/com/pm/patientservice/dto/PatientRequestDTO.java
@@ -4,7 +4,10 @@ import com.pm.patientservice.dto.validators.CreatePatientValidationGroup;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.Data;
 
+
+@Data
 public class PatientRequestDTO {
 
   @NotBlank(message = "Name is required")
@@ -25,48 +28,5 @@ public class PatientRequestDTO {
       "Registered date is required")
   private String registeredDate;
 
-  public @NotBlank(message = "Name is required") @Size(max = 100, message = "Name cannot exceed 100 characters") String getName() {
-    return name;
-  }
-
-  public void setName(
-      @NotBlank(message = "Name is required") @Size(max = 100, message = "Name cannot exceed 100 characters") String name) {
-    this.name = name;
-  }
-
-  public @NotBlank(message = "Email is required") @Email(message = "Email should be valid") String getEmail() {
-    return email;
-  }
-
-  public void setEmail(
-      @NotBlank(message = "Email is required") @Email(message = "Email should be valid") String email) {
-    this.email = email;
-  }
-
-  public @NotBlank(message = "Address is required") String getAddress() {
-    return address;
-  }
-
-  public void setAddress(
-      @NotBlank(message = "Address is required") String address) {
-    this.address = address;
-  }
-
-  public @NotBlank(message = "Date of birth is required") String getDateOfBirth() {
-    return dateOfBirth;
-  }
-
-  public void setDateOfBirth(
-      @NotBlank(message = "Date of birth is required") String dateOfBirth) {
-    this.dateOfBirth = dateOfBirth;
-  }
-
-  public String getRegisteredDate() {
-    return registeredDate;
-  }
-
-  public void setRegisteredDate(String registeredDate) {
-    this.registeredDate = registeredDate;
-  }
 
 }
