f2effa15a8a1a07e48c23e9a748ecb0c124c7843
오조벡 (CT00383) <ozodbek@nextree.io>
2025-08-26 17:00:43 +0900

    analytic-service added

--- patient-service/src/main/java/com/pm/patientservice/kafka/KafkaProducer.java
+++ patient-service/src/main/java/com/pm/patientservice/kafka/KafkaProducer.java
@@ -1,18 +1,35 @@
 package com.pm.patientservice.kafka;
 
 
+import com.pm.patientservice.model.Patient;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+import patient.events.PatientEvent;
 
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class KafkaProducer {
 
-    private final KafkaTemplate<String,byte[]> kafkaTemplate;
+    private final KafkaTemplate<String, byte[]> kafkaTemplate;
 
-
+    public void sendEvent(Patient patient) {
+        PatientEvent event = PatientEvent.newBuilder()
+                .setPatientId(patient.getId().toString())
+                .setName(patient.getName())
+                .setEmail(patient.getEmail())
+                .setEventTpe("PATIENT_CREATED")
+                .build();
+        try {
+            kafkaTemplate.send("patient", event.toByteArray());
+        } catch (Exception e) {
+            log.error("Error in sending PatientCreated event: {}", event);
+            log.error("Error log", e);
+        }
+    }
 
 
 }
