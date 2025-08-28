cbff1ac34b315f14b4fe9617214ebd9023ea6d1a
Ozodbek1997 <ozodbek.xomidbekov@gmail.com>
2025-08-28 11:16:04 +0900

    check Authorization

--- api-gateway/src/main/java/com/pm/apigateway/filter/JwtValidationGatewayFilterFactory.java
+++ api-gateway/src/main/java/com/pm/apigateway/filter/JwtValidationGatewayFilterFactory.java
@@ -1,6 +1,5 @@
 package com.pm.apigateway.filter;
 
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
@@ -10,9 +9,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Component
-
-public class JwtValidationGatewayFilterFactory extends
-        AbstractGatewayFilterFactory<Object> {
+public class JwtValidationGatewayFilterFactory extends AbstractGatewayFilterFactory<Object> {
 
     private final WebClient webClient;
 
@@ -24,10 +21,9 @@ public class JwtValidationGatewayFilterFactory extends
     @Override
     public GatewayFilter apply(Object config) {
         return (exchange, chain) -> {
-            String token =
-                    exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
-
-            if(token == null || !token.startsWith("Bearer ")) {
+            String token = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+            System.out.println("hello world" + token);
+            if (token == null || !token.startsWith("Bearer ")) {
                 exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
                 return exchange.getResponse().setComplete();
             }
@@ -37,7 +33,12 @@ public class JwtValidationGatewayFilterFactory extends
                     .header(HttpHeaders.AUTHORIZATION, token)
                     .retrieve()
                     .toBodilessEntity()
-                    .then(chain.filter(exchange));
+                    .then(chain.filter(exchange))
+                    .onErrorResume(error -> {
+                        // Handle validation errors
+                        exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                        return exchange.getResponse().setComplete();
+                    });
         };
     }
 }
